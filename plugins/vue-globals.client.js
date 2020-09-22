e100a004c3843fcf94ec9c43789874a236a87b14
Eric Howard <howard.eric.m@gmail.com>
2020-09-22 10:41:26 -0700

    adding v-balance-text directive

--- plugins/vue-globals.client.js
+++ plugins/vue-globals.client.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import inViewportDirective from 'vue-in-viewport-directive'
+import balanceTextDirective from 'vue-balance-text'
 
 require('intersection-observer')
 
 Vue.directive('in-viewport', inViewportDirective)
+Vue.directive('balance-text', balanceTextDirective)
