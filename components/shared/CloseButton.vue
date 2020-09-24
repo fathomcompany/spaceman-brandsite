7ef3dcd422801075f4a8b74c4d2c686f5b6d8b53
Eric Howard <howard.eric.m@gmail.com>
2020-09-24 11:37:47 -0700

    updating redirects

--- components/shared/CloseButton.vue
+++ components/shared/CloseButton.vue
@@ -3,7 +3,7 @@ CloseButton
 -->
 
 <template lang="pug">
-.CloseButton.fixed.right-0.top-0.text-6xl.font-hairline.z-50.mt-4.mr-4.origin-center.h-8.w-8.md_h-12.md_w-12
+.CloseButton.fixed.right-0.top-0.text-6xl.font-hairline.z-50.mt-4.mr-4.md_mt-5.md_mr-5.origin-center.h-8.w-8.md_h-12.md_w-12
   nuxt-link.inset-0.absolute(to="/")
     .bar.bg-white.absolute.h-full.transform.rotate-45.origin-center.left-50p
     .bar.bg-white.absolute.h-full.transform.-rotate-45.origin-center.left-50p
