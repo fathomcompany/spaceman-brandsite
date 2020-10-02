672bd5b7c813c6904385967810ed32d1829089d7
Eric Howard <howard.eric.m@gmail.com>
2020-10-02 10:41:11 -0700

    adjusting size of mobile first content block

--- components/blocks/MobileFirstContent.vue
+++ components/blocks/MobileFirstContent.vue
@@ -35,19 +35,24 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+mfcBreakpoint = (tablet + 200px)
 .MobileFirstContent
-  aspect-ratio 1, false
+  aspect-ratio 1.3/1, false
 
-  @media(max-width tablet)
-    aspect-ratio 0.6/1, false
+  @media(max-width mfcBreakpoint)
+    aspect-ratio 0.7/1, false
 
 .content-container
   width 40%
   max-width 550px
+  width 35%
   padding-right 8%
-  padding-top 8%
+  padding-top 5%
 
-  @media(max-width tablet)
-    width 60%
+  @media(max-width mfcBreakpoint)
+    width 50%
     padding-top 12%
+
+  @media(max-width mobile)
+    width 55%
 </style>
