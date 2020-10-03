4b229e4272c266435c9ba5cf896ec4e1a302e3d0
Eric Howard <howard.eric.m@gmail.com>
2020-10-02 17:23:18 -0700

    fixing going back from project pages

--- components/shared/CloseButton.vue
+++ components/shared/CloseButton.vue
@@ -4,7 +4,7 @@ CloseButton
 
 <template lang="pug">
 .CloseButton.fixed.right-0.top-0.text-6xl.font-hairline.z-50.mt-4.mr-4.md_mt-5.md_mr-5.origin-center.h-8.w-8.md_h-12.md_w-12
-  nuxt-link.inset-0.absolute(to="/")
+  a.inset-0.absolute(href="/" event="" @click="handleClick($event)")
     .bar.bg-white.absolute.h-full.transform.rotate-45.origin-center.left-50p
     .bar.bg-white.absolute.h-full.transform.-rotate-45.origin-center.left-50p
 </template>
@@ -13,8 +13,23 @@ CloseButton
 export default {
   name: 'CloseButton',
 
-  mounted() {
-    console.log(this.$router)
+  methods: {
+    handleClick(e) {
+      e.preventDefault()
+      e.stopPropagation()
+      const internalStart = this.$router.history._startLocation
+      const currentPath = this.$route.path
+
+      // When the start and current paths match,
+      // Use the default behaviour
+      if (currentPath === internalStart) {
+        this.$router.push('/')
+      }
+      // Otherwise simply go back, assuming you've come from another internal page
+      else {
+        this.$router.back()
+      }
+    }
   }
 }
 </script>
