f43c233bba334c7a54160650ebafc97480c8c567
Eric Howard <howard.eric.m@gmail.com>
2020-10-20 17:38:30 -0700

    heading content support image aspect

--- components/blocks/HeadingContent.vue
+++ components/blocks/HeadingContent.vue
@@ -6,13 +6,20 @@ HeadingContent
 .HeadingContent.h-gutter.relative(
   :class="{ 'text-center': alignment === 'Center' }"
 )
+  .aspect-item(
+    v-if="block.imageAspectSizing && aspect !== 0"
+    :style="{ 'padding-top': aspect }"
+  )
+
   ResponsiveMedia.absolute.inset-0(
+    v-if="block.imageDesktop || block.imageMobile"
+    ref="content"
     :image="block.imageDesktop"
     :mobileImage="block.imageMobile"
     :fill="true"
   )
 
-  .v-gutter-huge.relative(
+  .v-gutter-huge.relative.inline-block(
     :class="{ 'content-wide': alignment === 'Left', 'content': alignment === 'Center' }"
   )
     ContentfulRichText(:content="block.content")
@@ -36,16 +43,43 @@ export default {
     }
   },
 
+  data() {
+    return {
+      ready: false
+    }
+  },
+
   computed: {
     alignment() {
       return get(this.block, 'alignment', 'Center')
+    },
+
+    aspect() {
+      if (!this.ready) return 0
+
+      if (!this.$refs.content) return 0
+
+      console.log(this.$refs.content.aspect)
+      return `${(1 / this.$refs.content.aspect) * 100}%`
     }
+  },
+
+  mounted() {
+    this.ready = true
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.content-wide
-  .ContentfulRichText
-    max-width 1300px
+.HeadingContent
+  font-size 0
+
+.aspect-item
+  height 100%;
+  display inline-block
+  vertical-align top
+
+.ContentfulRichText
+  font-size initial
+  max-width 1300px
 </style>
