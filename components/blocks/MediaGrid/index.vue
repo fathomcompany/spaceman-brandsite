f80d3c1b8e083ee6f8e631ed963b1b8ebed30d37
Eric Howard <howard.eric.m@gmail.com>
2020-10-28 11:02:12 -0700

    Square media grid laid out

--- components/blocks/MediaGrid/index.vue
+++ components/blocks/MediaGrid/index.vue
@@ -5,18 +5,9 @@ MediaGrid
 <template lang="pug">
 .MediaGrid.bg-offblack.h-gutter.py-8.md_py-12.xl_py-20
   .content-wide.relative
-    .grid(
-      v-masonry,
-      :maxCols='3',
-      :maxColWidth='800',
-      :gap='10',
-      transition-duration='0s'
-      :destroy-delay="5000"
-    )
-      .item(
-        v-masonry-tile,
+    .flex.flex-wrap
+      .item.Square(
         v-for='(asset, index) in block.assets',
-        :class='[asset.aspect || "Square"]'
       )
         .item-wrapper.relative
           .media-container.absolute.inset-0.flex.justify-center.items-center.bg-black.overflow-hidden
@@ -59,7 +50,6 @@ export default {
     },
 
     onActiveSet(index) {
-      console.log('setting active:', index)
       this.activeIndex = index
     }
   }
@@ -70,49 +60,25 @@ export default {
 gap = (20px / 2.5)
 
 .MediaGrid
-  .content-wide
-    width 'calc(100% + %s)' % gap
+  //
 
-  .grid
-    margin-left (gap / -2)
+.content-wide
+  width 'calc(100% + %s)' % gap
+  margin-left (gap / -2)
 
-  .media-container
-    margin gap
+.media-container
+  margin gap
 
-  .item
-    @media (max-width tablet)
-      width 100%
+.item
+  .item-wrapper
+    padding-bottom 'calc(100% - %s)' % (gap / 2)
 
-  .item.Tall
-    @media (min-width (tablet + 1px))
-      width 33.3333%
+  @media (min-width (tablet-landscape + 1px))
+    width 33.3333%
 
-      .item-wrapper
-        padding-bottom 'calc(200% - %s)' % gap
+  @media(max-width tablet-landscape)
+    width 50%
 
-    @media (max-width tablet)
-      .item-wrapper
-        padding-bottom 200%
-
-  .item.Square
-    @media (min-width (tablet + 1px))
-      width 33.3333%
-
-      .item-wrapper
-        padding-bottom 'calc(100% - %s)' % (gap / 2)
-
-    @media (max-width tablet)
-      .item-wrapper
-        padding-bottom 100%
-
-  .item.Wide
-    @media (min-width (tablet + 1px))
-      width 66.6666%
-
-      .item-wrapper
-        padding-bottom 'calc(50% - %s)' % (gap / 2)
-
-    @media (max-width tablet)
-      .item-wrapper
-        padding-bottom 56.25%
+  @media (max-width mobile-landscape)
+    width 100%
 </style>
